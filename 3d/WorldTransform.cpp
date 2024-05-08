b81078ab410d28f095a89bbefd1260e50fc3888a
TsubokiHayato <146918548+TsubokiHayato@users.noreply.github.com>
2024-05-08 16:35:46 +0900

    kannseeei

--- 3d/WorldTransform.cpp
+++ 3d/WorldTransform.cpp
@@ -3,3 +3,9 @@
 #include "WorldTransform_MT.h"
 
 void WorldTransform::UpdateMatrix(WorldTransform* world) { MakeAffineMatrix(world->scale_, world->rotation_, world->translation_); }
+
+Matrix4x4 WorldTransform::UpdateMatMatrix(WorldTransform* world) { 
+	Matrix4x4 result = {};
+	result=MakeAffineMatrix(world->scale_, world->rotation_, world->translation_); 
+	return result;
+}
