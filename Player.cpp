f28eab69e8134727d077e0384f200dbaffcff6de
TsubokiHayato <146918548+TsubokiHayato@users.noreply.github.com>
2024-06-05 15:11:25 +0900

    a

--- Player.cpp
+++ Player.cpp
@@ -34,7 +34,36 @@ void Player::Initialize(Model* model, ViewProjection* viewProjection) {
 // 更新
 void Player::Update() {
 
-	
+	// playerの移動ベクトル
+	Vector3 move = {0.0f, 0.0f, 0.0f};
+
+	// キャラクターの移動速さ
+	const float kCharaSpeed = 0.2f;
+
+	if (input_->PushKey(DIK_A)) {
+		move.x -= kCharaSpeed;
+	} else if (input_->PushKey(DIK_D)) {
+		{ move.x += kCharaSpeed; }
+	}
+
+	if (input_->PushKey(DIK_S)) {
+		move.y -= kCharaSpeed;
+	} else if (input_->PushKey(DIK_W)) {
+		{ move.y += kCharaSpeed; }
+	}
+
+	ImGui::SliderFloat3("Player Position", &worldTransform_.translation_.x, -600.0f, 600.0f);
+
+	// 移動限界座標
+	const float kMoveLimitX = 35.0f;
+	const float kMoveLimitY = 19.0f;
+
+	worldTransform_.translation_.x = max(worldTransform_.translation_.x, -kMoveLimitX);
+	worldTransform_.translation_.x = min(worldTransform_.translation_.x, +kMoveLimitX);
+	worldTransform_.translation_.y = max(worldTransform_.translation_.y, -kMoveLimitY);
+	worldTransform_.translation_.y = min(worldTransform_.translation_.y, +kMoveLimitY);
+
+	worldTransform_.translation_ += move;
 	// 行列計算
 	worldTransform_.UpdateMatrix();
 
