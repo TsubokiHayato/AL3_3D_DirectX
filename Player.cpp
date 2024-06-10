a9ddd42726a01c49b170b38afe12d13f62b1f072
TsubokiHayato <146918548+TsubokiHayato@users.noreply.github.com>
2024-06-10 15:43:05 +0900

    GetWorldPos修正済

--- Player.cpp
+++ Player.cpp
@@ -36,8 +36,8 @@ Vector3 Player::GetWorldPos() {
 	Vector3 worldPos = {};
 
 	worldPos.x = worldTransform_.matWorld_.m[3][0];
-	worldPos.x = worldTransform_.matWorld_.m[3][1];
-	worldPos.x = worldTransform_.matWorld_.m[3][2];
+	worldPos.y = worldTransform_.matWorld_.m[3][1];
+	worldPos.z = worldTransform_.matWorld_.m[3][2];
 	
 	
 	
