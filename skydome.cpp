87eb5ec77741b59f7eb1d627d7bb152e76b22bcf
TsubokiHayato <146918548+TsubokiHayato@users.noreply.github.com>
2024-06-12 15:37:05 +0900

    .cpp追加

--- skydome.cpp
+++ skydome.cpp
@@ -1,3 +1,18 @@
-#include "skydome.h"
+#include "Skydome.h"
 
-void Skydome::Initialize(Model* model, ViewProjection* viewProjection) {}
+#include <cassert>
+
+void Skydome::Initialize(Model* model, ViewProjection* viewProjection) {
+	assert(model);
+
+	// 3Dモデルの作成
+	Sky_Model_ = model;
+
+	// ワールドトランスフォームの初期化
+	Sky_worldTransform_.Initialize();
+
+	Sky_viewProjection_ = viewProjection;
+}
+
+void Skydome::Update() { Sky_worldTransform_.TransferMatrix(); }
+void Skydome::Draw() { Sky_Model_->Draw(Sky_worldTransform_, *Sky_viewProjection_); }
