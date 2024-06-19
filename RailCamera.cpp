f643b3417d15a032812c35b6ff156cef64e74fe1
TsubokiHayato <146918548+TsubokiHayato@users.noreply.github.com>
2024-06-19 15:36:49 +0900

    a

--- RailCamera.cpp
+++ RailCamera.cpp
@@ -1,25 +1,20 @@
 #include "RailCamera.h"
-#include<WorldTransform_MT.h>
+#include <WorldTransform_MT.h>
 
-#include<ImGuiManager.h>
+#include <ImGuiManager.h>
 void RailCamera::Initialize(Vector3 cameraPos, Vector3 cameraRotate) {
 
 	worldTransform_.translation_ = cameraPos;
 	worldTransform_.rotation_ = cameraRotate;
 
 	viewProjection_.Initialize();
-
-
 }
 
-void RailCamera::Update() { 
-	
+void RailCamera::Update() {
+
 	worldTransform_.translation_.z += 0.01f;
 	worldTransform_.rotation_.z += 0.01f;
 
-	worldTransform_.UpdateMatrix();
-
 	viewProjection_.matView = Inverse(worldTransform_.matWorld_);
-
-
+	//worldTransform_.UpdateMatrix();
 }
