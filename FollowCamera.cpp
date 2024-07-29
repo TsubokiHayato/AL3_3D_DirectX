7afb3f480fd814780610b39a750ad0fc44ffadc9
TsubokiHayato <146918548+TsubokiHayato@users.noreply.github.com>
2024-07-29 14:14:23 +0900

    s

--- FollowCamera.cpp
+++ FollowCamera.cpp
@@ -10,39 +10,34 @@ inline Vector3 TransformNormal(const Vector3& vector, const Matrix4x4& matrix) {
 }
 void FollowCamera::Initialize() {
 	
-	viewProjection_.Initialize(); }
+	viewProjection_.Initialize(); 
+
+}
 
 void FollowCamera::Update() {
 
-
+	
 	XINPUT_STATE joyState;
 
 	if (Input::GetInstance()->GetJoystickState(0, joyState)) {
 
 		const float speed = 0.1f;
 
-		viewProjection_.rotation_.y += (float)joyState.Gamepad.sThumbRX/ SHRT_MAX * speed;
-
+		viewProjection_.rotation_.y += (float)joyState.Gamepad.sThumbRX / SHRT_MAX * speed;
 	}
 
 	if (target_) {
 		Vector3 offset{0.0f, 2.0f, -10.0f};
 
 		Vector3 cameraRotationAngles = viewProjection_.rotation_;
-		Matrix4x4 rotateXMatrix = MakeRotateXMatrix(cameraRotationAngles.x);
+
 		Matrix4x4 rotateYMatrix = MakeRotateYMatrix(cameraRotationAngles.y);
-		Matrix4x4 rotateZMatrix = MakeRotateZMatrix(cameraRotationAngles.z);
-		Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
 
-		offset = TransformNormal(offset,rotateXYZMatrix );
-
+		offset = TransformNormal(offset, rotateYMatrix);
 
 		viewProjection_.translation_ = target_->translation_ + offset;
 	}
 
-	
-
 	viewProjection_.UpdateMatrix();
-	viewProjection_.TransferMatrix();
 	
 }
