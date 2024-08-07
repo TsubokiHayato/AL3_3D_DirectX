93c6700f6fc3d14dab3cbb93b0709bef2d93b19e
TsubokiHayato <146918548+TsubokiHayato@users.noreply.github.com>
2024-08-07 12:59:59 +0900

    a

--- LockOn.cpp
+++ LockOn.cpp
@@ -1,10 +1,12 @@
 #include "LockOn.h"
+#include"TextureManager.h"
 
 void LockOn::Initialize() { 
 	viewProjection_->Initialize();
 	worldTransform_.Initialize();
 	lockOnMark_->Initialize();
-	lockOnMark_->Create()
+	uint32_t textureReticle = TextureManager::Load("2D_Reticle.png");
+	lockOnMark_ = Sprite::Create(textureReticle, {}, {1.0f, 1.0f, 1.0f, 1.0f}, anchorPoint);
 
 
 }
